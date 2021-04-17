09a1193971795cba3f97d35f4df60f82c0e076a8
Yan <yans@yancomm.net>
2021-04-16 23:53:18 -0700

    acquiesce to fish's crazy demands

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@
 O="$1"
 PKG="$2"
 
-docker run -i --rm -v $PWD:/g gbuilder /g/build.sh "$PKG" "-g -O$O -fno-eliminate-unused-debug-types" /g/out-$O
+docker run -i --rm -v $PWD:/g gbuilder /g/build.sh "$PKG" "-g -O$O -fcf-protection=none -fno-eliminate-unused-debug-types" /g/out-$O
